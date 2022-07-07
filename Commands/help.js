8e0ac819561cc6878cb8b2af0dc18dc27597f318
Fkhorasani <46391958+Fkhorasani@users.noreply.github.com>
2022-07-07 04:12:50 +0000

    update help

--- Commands/help.js
+++ Commands/help.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js")
 module.exports.run = (client, message, args) =>{
   const commands = client.commands.map(command => command.name).join("\n ")
+  const size = client.commands.size + 2;
   const embed = new Discord.MessageEmbed()
-  .setTitle(`Total commands: ${client.commands.size}`)
+  .setTitle(`Total commands: ${size}`)
   .addField("command",commands, true)
   .addField("command","gif\nsticker", true)
   .setFooter("My Prefix is + \nBot made by FuyumiHirai#3684")
