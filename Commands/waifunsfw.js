6b9b4def020979d1730182938d70adf1ca8281a4
Fkhorasani <46391958+Fkhorasani@users.noreply.github.com>
2022-07-07 04:20:54 +0000

    nsfw controll for waifunsfw

--- Commands/waifunsfw.js
+++ Commands/waifunsfw.js
@@ -2,11 +2,13 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 
 module.exports.run = async (client, message, args) =>{
-    const url = "https://api.waifu.pics/nsfw/waifu"
-    const res = await fetch(url);
-    const json = await res.json();
-
-    message.channel.send(`Here you go --> ${json.url}`);
+    if(message.channel.nsfw) {
+      const url = "https://api.waifu.pics/nsfw/waifu"
+      const res = await fetch(url);
+      const json = await res.json();
+      message.channel.send(`Here you go --> ${json.url}`);
+    }
+  else message.channel.send("This channel is SFW.");
 }
 
 module.exports.name = "waifunsfw"
